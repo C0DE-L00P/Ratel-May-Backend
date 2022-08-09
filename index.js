3c3bdb91a43d4fcc5f9c17ef7d108c9769d5a169
Mohamed Gamal <43136060+C0DE-L00P@users.noreply.github.com>
2022-08-09 08:48:45 +0200

    Solving PORT recognition in heroku

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const cors = require("cors");
 const helmet = require("helmet");
 const Session = require("./models/sessionSchema");
 
-const PORT = process.env.SERVER_PORT || 5000;
+const PORT = process.env.PORT || 5000;
 const HOST = "0.0.0.0"
 
 //mongo Connection
