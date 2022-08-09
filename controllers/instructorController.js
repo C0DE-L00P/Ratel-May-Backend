2a2c31c6e74e2b44e87ea7ff45adfe485f743ac2
Mohamed Gamal <jaiden90@gmail.com>
2022-08-09 16:13:14 +0200

    Resolve sendStatus problem

--- controllers/instructorController.js
+++ controllers/instructorController.js
@@ -8,12 +8,32 @@ const bcrypt = require("bcrypt");
 
 const instructors_get_id = async (mreq, mres) => {
   let res = await Instructor.findById(mreq.params.id)
-  .populate("students",{ name: 1, email: 1,_id: 1,has_whatsapp: 1, mobile: 1})
-  .populate("sessions",{ room_id: 1, created_at: 1,ended_at: 1,_id: 1, participants: 1,is_live:1, previously_reached: 1,recently_reached:1})
-  .populate("evaluations",{ student: 1, _id: 1,instructor_evaluation: 1,problems: 1})
-  .select({password: 0});
+    .populate("students", {
+      name: 1,
+      email: 1,
+      _id: 1,
+      has_whatsapp: 1,
+      mobile: 1,
+    })
+    .populate("sessions", {
+      room_id: 1,
+      created_at: 1,
+      ended_at: 1,
+      _id: 1,
+      participants: 1,
+      is_live: 1,
+      previously_reached: 1,
+      recently_reached: 1,
+    })
+    .populate("evaluations", {
+      student: 1,
+      _id: 1,
+      instructor_evaluation: 1,
+      problems: 1,
+    })
+    .select({ password: 0 });
   if (res == undefined || res == null) {
-    mres.sendStatus(404).json({ message: "No instructor with such id" });
+    mres.status(404).json({ message: "No instructor with such id" });
     return;
   }
   //TODO: ربما انتا مش هتحتاج ترجع كل دا قدام
@@ -24,11 +44,10 @@ const instructors_get_id = async (mreq, mres) => {
   mres.json(res);
 };
 
-const instructors_put_id = (mreq, mres) => {  
-
+const instructors_put_id = (mreq, mres) => {
   //TODO: handle password change
-  delete mreq.body.password
-  delete mreq.body.email
+  delete mreq.body.password;
+  delete mreq.body.email;
 
   Instructor.findByIdAndUpdate(mreq.params.id, mreq.body, function (err, docs) {
     if (err) {
@@ -39,48 +58,46 @@ const instructors_put_id = (mreq, mres) => {
     try {
       let updatedItem = { ...docs._doc, ...mreq.body };
       mres.status(200).json(updatedItem);
-      
     } catch (error) {
       mres.status(400).json({ message: error });
     }
-  }).select({password: 0});
+  }).select({ password: 0 });
 };
 
-const instructors_put_id_newpass = (mreq,mres)=> {
+const instructors_put_id_newpass = (mreq, mres) => {
   //TODO: شوف الخطوات دي يسطا بعدين
   //Check if the changed data is the password as it must be hashed first
   // bcrypt.compare(mreq.body.password)
   //كنت هنا باعدل الحاجات ماشي ع الخطوات
 
-  //Check if he forgot the previous password 
+  //Check if he forgot the previous password
   //if not then he must send the previous password and the new one
-  if(mreq.body)
-  //First: hash the password and post it back in the object
+  if (mreq.body)
+    //First: hash the password and post it back in the object
 
-  bcrypt.hash(mreq.body.password, 10, function (err, hash) {
+    bcrypt.hash(mreq.body.password, 10, function (err, hash) {
+      if (err != null) {
+        mres.json({ message: err });
+        return;
+      }
+      mreq.body.password = hash;
 
-    if (err != null) {
-      mres.json({ message: err });
-      return;
-    }
-    mreq.body.password = hash;
-
-    //Then: Save the data in the database
-    const instructor = new Instructor(mreq.body);
-    instructor
-      .save()
-      .then((res_cat) => {
-        mres.json(res_cat);
-      })
-      .catch((err) => {
-        mres.sendStatus(400).json({ message: err });
-      });
-  });
-}
+      //Then: Save the data in the database
+      const instructor = new Instructor(mreq.body);
+      instructor
+        .save()
+        .then((res_cat) => {
+          mres.json(res_cat);
+        })
+        .catch((err) => {
+          mres.status(400).json({ message: err });
+        });
+    });
+};
 
 const instructors_delete_id = (mreq, mres) => {
   Instructor.findByIdAndDelete(mreq.params.id, function (err) {
-    if (err) mres.sendStatus(404).json({ message: err });
+    if (err) mres.status(404).json({ message: err });
     else mres.sendStatus(200);
   });
 };
@@ -88,15 +105,10 @@ const instructors_delete_id = (mreq, mres) => {
 // --------------------- General
 
 const instructors_post = (mreq, mres) => {
-
   //First: hash the password and post it back in the object
 
   bcrypt.hash(mreq.body.password, 10, function (err, hash) {
-
-    if (err != null) {
-      mres.json({ message: err });
-      return;
-    }
+    if (err != null) return mres.json({ message: err });
     mreq.body.password = hash;
 
     //Then: Save the data in the database
@@ -104,11 +116,11 @@ const instructors_post = (mreq, mres) => {
     instructor
       .save()
       .then((res_cat) => {
-        delete res_cat.password
+        delete res_cat.password;
         mres.json(res_cat);
       })
       .catch((err) => {
-        mres.sendStatus(400).json({ message: err });
+        mres.status(400).json({ message: err });
       });
   });
 };
@@ -119,10 +131,16 @@ const instructors_get = (mreq, mres) => {
     //TODO: add students count and sessions count
     let que = mreq.query.Name || mreq.query.name;
     Instructor.find({ name: que })
-    .populate('students',{ name: 1, email: 1,_id: 1,has_whatsapp: 1, mobile: 1})
+      .populate("students", {
+        name: 1,
+        email: 1,
+        _id: 1,
+        has_whatsapp: 1,
+        mobile: 1,
+      })
       .select({ password: 0 })
       .then((filt_insts) => {
-        if (filt_insts.length <= 0) 
+        if (filt_insts.length <= 0)
           return mres
             .status(404)
             .json({ message: "No instructor found with such a name" });
--- models/studentSchema.js
+++ models/studentSchema.js
@@ -3,13 +3,13 @@ const Schema = mongoose.Schema;
 
 //TODO: add other programs support to this schema ex: Noor Bayan and multiprogram subscriptions
 const studentSchema = new Schema({
-  name: {type: String,required: true},
-  age: {type: Number,required: [true,"You must put an age"]},
-  gender: {type: String,required: true,enum: ["Male", "Female"]},
+  name: { type: String, required: true },
+  age: { type: Number, required: [true, "You must put an age"] },
+  gender: { type: String, required: true, enum: ["Male", "Female"] },
+  state: { type: String, required: true },
   mobile: String,
-  email: {type: String,required: [true,"You must put an email"]},
-  password: {type: String,required: true},
-  state: {type: String, required: true},
+  email: { type: String, required: [true, "You must put an email"] },
+  password: { type: String, required: true },
   notes_in_book: [
     {
       page: Number,
@@ -19,20 +19,39 @@ const studentSchema = new Schema({
       content: String,
     },
   ],
-  sessions: [{ type: Schema.Types.ObjectId, ref: "Session"}],
-  instructor: { type: Schema.Types.ObjectId, ref: "Instructor"},
-  program_prefs: {
+  subscription_state: {
     type: String,
+    default: function () {
+      return this.instructor === undefined ? "Pending" : "Active";
+    },
+    enum: ["Pending", "Active", "Onhold", "Cancelled"],
+  },
+  sessions: [{ type: Schema.Types.ObjectId, ref: "Session" }],
+  instructor: { type: Schema.Types.ObjectId, ref: "Instructor" },
+  program_prefs: {
+    type: {
+      type: String,
+      enum: ["Memorizing", "Recitation", "Noor Bayan"],
+      default: "Memorizing",
+    },
     sessions_in_week: Number,
     pref_days: [String],
     pref_times_of_day: [[Number]],
     plan: [],
   },
   certificate: String,
-  reached: String,
   started_from_surah: String,
-  has_whatsapp: Boolean,
-  started_in: {type: Date ,default: Date.now},
+  reached_surah: String,
+  whatsapp_number: String,
+  started_in: {
+    type: Date,
+    default: function () {
+      if (this.subscription_state == "active") {
+        return Date.now();
+      }
+      return null;
+    },
+  },
 });
 
 const Student = new mongoose.model("Student", studentSchema);
